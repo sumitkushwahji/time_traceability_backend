0ef949b841c39df9c11cb97b716d4277347768c8
sumit kushwah <sumitkushwahji@gmail.com>
2025-04-15 15:57:51 +0530

    added unversioned file

---
+++ src/test/java/com/time/tracealibility/TracealibilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time.tracealibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracealibilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
